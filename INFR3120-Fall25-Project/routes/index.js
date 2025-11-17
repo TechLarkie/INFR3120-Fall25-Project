be106caa461f21298a56c2635ad6419c1ad6cb2c
alexjaruczik <alex.jaruczik@ontariotechu.net>
2025-11-16 21:48:35 -0500

    Added more text to display navigation between templates.

--- INFR3120-Fall25-Project/routes/index.js
+++ INFR3120-Fall25-Project/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'DrawThree' });
+});
+router.get('/store', function(req, res, next) {
+  res.render('store', { title: 'DrawThree | Find a Store' });
+});
+router.get('/tournament', function(req, res, next) {
+  res.render('tournament', { title: 'DrawThree \ Tournaments' });
 });
 
 module.exports = router;
