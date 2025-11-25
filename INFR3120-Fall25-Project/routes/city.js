4f26951dd6b3baf744bcebfd38246722f9453fab
Sh1ro002 <91203150+Sh1ro002@users.noreply.github.com>
2025-11-25 18:31:59 -0500

    Update error message for city not found n documentation

--- INFR3120-Fall25-Project/routes/city.js
+++ INFR3120-Fall25-Project/routes/city.js
@@ -3,19 +3,19 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 
-// pick-a-city page
+//IF U WANT A STORE... U NEED A CITY. YK??
 router.get('/find', (req, res) => {
   const cities = ['toronto', 'vaughan', 'oshawa'];
   res.render('pick-city', { cities });
 });
 
-// dynamic route to load city file
+//this will load the city file u chose
 router.get('/:city', (req, res) => {
   const city = req.params.city.toLowerCase();
   const filePath = path.join(__dirname, '..', 'data', `${city}.json`);
 
   if (!fs.existsSync(filePath)) {
-    return res.status(404).send("City not found.");
+    return res.status(404).send("Error 404: City Not Found :(");
   }
 
   const stores = JSON.parse(fs.readFileSync(filePath));
